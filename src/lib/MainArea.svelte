48d011fcf98d5d7f43ace8fd379b5b0baf340b34
idomshi <56523771+idomshi@users.noreply.github.com>
2023-06-24 21:16:17 +0900

    スライダーを作ります

--- src/lib/MainArea.svelte
+++ src/lib/MainArea.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
-function onclick (): void {
-  console.log("clicked!")
-}
+  function onclick(): void {
+    console.log("clicked!");
+  }
+
+  let sliderValue = 0;
+  $: posX = ((260 - 40) * sliderValue) / (100 - 0) + 40;
+
+  function posToVal (pos: number): number {
+    return Math.max(0, Math.min(100, (pos - 40) * (100 - 0) / (260 - 40)))
+  }
+
+  let isDragging = false;
+  function dragstart(e: PointerEvent) {
+    isDragging = true;
+  }
+
+  function dragmove(e: PointerEvent) {
+    if (!isDragging) return
+    sliderValue = posToVal(e.offsetX)
+  }
+
+  function dragend(e: PointerEvent) {
+    if (!isDragging) return
+    sliderValue = posToVal(e.offsetX)
+    isDragging = false
+  }
 </script>
 
 <main class="h-full grow bg-slate-50">
@@ -13,4 +36,24 @@ function onclick (): void {
   >
     push this button
   </button>
+
+  <svg
+    width="300" height="80"
+    on:pointerleave={dragend}
+  >
+    <line x1="40" y1="40" x2="260" y2="40" stroke="#ccc" stroke-width="4" />
+    <circle
+      on:pointerdown={dragstart}
+      on:pointermove={dragmove}
+      on:pointerup={dragend}
+      cx={posX}
+      cy="40"
+      r="8"
+      stroke="#888"
+      stroke-width="4"
+      fill="#eee"
+    />
+  </svg>
+
+  <p>{sliderValue}</p>
 </main>
