92e005a586152e94b0cdd0ed601388dec9dca093
idomshi <56523771+idomshi@users.noreply.github.com>
2023-06-24 20:26:24 +0900

    押されるとconsoleに文字列を吐くだけのボタンを作ります

--- src/lib/MainArea.svelte
+++ src/lib/MainArea.svelte
@@ -1,3 +1,16 @@
+<script lang="ts">
+function onclick (): void {
+  console.log("clicked!")
+}
+</script>
+
 <main class="h-full grow bg-slate-50">
   <p>main area</p>
+  <button
+    type="button"
+    class="px-4 h-8 bg-slate-300 border-2 border-slate-400 rounded"
+    on:click={onclick}
+  >
+    push this button
+  </button>
 </main>
